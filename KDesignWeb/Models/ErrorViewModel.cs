c213d6df8145c639b083ab07d8f4d33ee91dc413
whoamiloathe <Dimainet.ru@gmail.com>
2023-09-27 10:16:45 +0300

    Add project files.

---
+++ KDesignWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KDesignWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
